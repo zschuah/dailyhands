c7d6fbcc089ee44b25ab189391e374e9c556e62b
zschuah <zschuah@hotmail.com>
2026-06-02 12:11:49 +0800

    animated emojis

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -12,8 +12,19 @@ export async function loader({}: Route.LoaderArgs) {}
 
 export default function Home() {
   return (
-    <div className="bg-zinc-300 min-h-screen">
-      <h2 className="text-5xl">Home</h2>
+    <div className="bg-zinc-300 min-h-screen grid place-items-center">
+      <div className="text-center flex flex-col items-center bg-zinc-200 p-8 rounded-3xl">
+        <h2 className="text-5xl">Hello there</h2>
+        <p>Welcome! Welcome!</p>
+
+        <img
+          className="w-60"
+          src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f44b/512.gif"
+          alt="👋"
+        />
+
+        <input className="input input-ghost pointer-events-none" type="text" />
+      </div>
     </div>
   );
 }
--- app/routes/under-construction.tsx
+++ app/routes/under-construction.tsx
@@ -21,13 +21,26 @@ export async function clientAction({ request }: Route.ClientActionArgs) {
 
 export default function UnderConstruction() {
   return (
-    <div className="bg-zinc-300 min-h-screen">
-      <h2 className="text-5xl">UnderConstruction</h2>
+    <div className="bg-zinc-300 min-h-screen grid place-items-center">
+      <div className="text-center flex flex-col items-center bg-zinc-200 p-8 rounded-3xl">
+        <h2 className="text-5xl">Sorry!</h2>
+        <p>This site is not ready yet.</p>
 
-      <Form method="POST">
-        <input className="input" type="text" name="konami" />
-        <button className="btn">Submit</button>
-      </Form>
+        <img
+          className="w-60"
+          src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f648/512.gif"
+          alt="🙈"
+        />
+
+        <Form method="POST">
+          <input
+            className="input input-ghost"
+            type="text"
+            name="konami"
+            autoComplete="off"
+          />
+        </Form>
+      </div>
     </div>
   );
 }
