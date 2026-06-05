3f5b860278662fb8c769e209015e941c5e75d8d6
zschuah <zschuah@hotmail.com>
2026-06-05 17:44:13 +0800

    added public ico and waving

--- app/utils/meta.ts
+++ app/utils/meta.ts
@@ -7,8 +7,16 @@ export function createMeta({
   title,
   description = "Practice SgSL here!",
 }: Props) {
+  const fullTitle = `DailyHands | ${title}`;
+
   return [
-    { title: `DailyHands | ${title}` },
+    { title: fullTitle },
     { name: "description", content: description },
+
+    // Open Graph
+    { property: "og:type", content: "website" },
+    { property: "og:title", content: fullTitle },
+    { property: "og:description", content: description },
+    { property: "og:image", content: "/waving.png" },
   ];
 }
