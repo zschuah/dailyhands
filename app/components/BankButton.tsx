0a645135ae2c33ab708c517a7c0fd36573ce53eb
zschuah <zschuah@hotmail.com>
2026-06-17 21:57:26 +0800

    fix: adjust button width

--- app/components/BankButton.tsx
+++ app/components/BankButton.tsx
@@ -42,7 +42,10 @@ const BankButton = ({
     <div
       ref={ref}
       onClick={handleButtonClick}
-      className={twMerge("btn relative min-h-fit", isOpen && "btn-active z-10")}
+      className={twMerge(
+        "btn relative min-h-fit md:max-w-48",
+        isOpen && "btn-active z-10",
+      )}
     >
       {isOpen && (
         <div
