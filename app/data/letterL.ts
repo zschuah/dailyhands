d9634241614535e25d5ae58e0a20c8c922b73a4b
zschuah <zschuah@hotmail.com>
2026-06-19 11:01:48 +0800

    feat: added till landslide

--- app/data/letterL.ts
+++ app/data/letterL.ts
@@ -13,5 +13,25 @@ export const letterL: SignProps[] = [
     name: "Lakeside (Place)",
     images: getImages("lakeside", "jpg"),
     link: "https://blogs.ntu.edu.sg/sgslsignbank/word/?frm-word=Lakeside%20(Place)",
+    tags: ["place"],
+  },
+  {
+    id: generateId("Lamb (Animal)"),
+    name: "Lamb (Animal)",
+    images: getImages("lamb", "png"),
+    link: "https://blogs.ntu.edu.sg/sgslsignbank/word/?frm-word=Lamb",
+    tags: ["animal"],
+  },
+  {
+    id: generateId("Lamp"),
+    name: "Lamp",
+    images: getImages("lamp", "png"),
+    link: "https://blogs.ntu.edu.sg/sgslsignbank/word/?frm-word=Lamp",
+  },
+  {
+    id: generateId("Landslide"),
+    name: "Landslide",
+    images: getImages("landslide", "jpg"),
+    link: "https://blogs.ntu.edu.sg/sgslsignbank/word/?frm-word=Landslide",
   },
 ];
